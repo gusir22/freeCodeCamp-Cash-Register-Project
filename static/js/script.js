1708ef381c35fef6028f79e1becb9ea6e36dfd57
Gustavo Rivero <gusir22@gmail.com>
2025-04-20 12:14:01 -0400

    Updated external JS script with freeCodeCamp start point code

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,12 @@
-console.log("I am a cash register!");
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
