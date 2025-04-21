ce9ed061ff44aa4654c9bfd49341e469737eb262
Gustavo Rivero <gusir22@gmail.com>
2025-04-20 21:33:01 -0400

    Created global cash variable for input user value

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 let price = 1.87;
+let cash; // init empty cash input user value
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -18,10 +19,25 @@ function displayPrice() {
     document.getElementById('price-output').innerText = `$${price}`;
 }
 
+function displayResults() {
+    /* This function formats and outputs the result message to the
+    #change-due element. */
+
+    let resultMessage = `<p>Status: Working<p>`;
+
+    resultMessage += `<p>Cash Value: ${cash}</p><br>`; // display cash variable value
+
+    document.getElementById('change-due').innerHTML = resultMessage;
+}
+
 function processPayment(form) {
     /* This function processes the main algorythm to assist in the
     purchase and provide the user their change.
     It is triggered with the #purchase-btn onclick event. */
 
     console.log("Processing Payment ...\nPlease wait ...");
+
+    cash = form.cash.value;
+
+    displayResults(); // print results
 }
