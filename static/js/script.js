a73118112cd09878a7bd63c594601e9d369c64a2
Gustavo Rivero <gusir22@gmail.com>
2025-04-20 22:50:08 -0400

    Updated calcChange() to tally change amounts per denomination required and store in change obj

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,21 @@ let cid = [ // array provided by freeCodeCamp project
   ['ONE HUNDRED', 100]
 ];
 
-var change = new Object() // init change object
+// init change object
+var change = {
+    'total': 0.00,
+    'centsTotal': 0.00,
+    'dollarsTotal': 0.00,
+    'PENNY': 0.00,
+    'NICKEL': 0.00,
+    'DIME': 0.00,
+    'QUARTER': 0.00,
+    'ONE': 0.00,
+    'FIVE': 0.00,
+    'TEN': 0.00,
+    'TWENTY': 0.00,
+    'ONEHUNDRED': 0.00,
+} 
 let resultMessage; // init empty global result message
 
 
@@ -22,11 +36,48 @@ function calcChange() {
     we have available in the cash in drawer (cid) and saves them in the global
     change object */
 
-    change.total = cash - price; // save total change amount to obj
+    change.total = cash - price; // update total change amount to obj
 
     // separate change into cents and dollar parts
     change.centsTotal = change.total - Math.trunc(change.total); // store cents total to obj
     change.dollarsTotal = Math.trunc(change.total); // store dollars total to obj
+
+    // tally cent denominations until there is less than a penny left
+    do {
+        if (change.centsTotal >= 0.25) {
+            change.QUARTER += 0.25;
+            change.centsTotal -= 0.25;
+        } else if (change.centsTotal >= 0.10) {
+            change.DIME += 0.10;
+            change.centsTotal -= 0.10;
+        } else if (change.centsTotal >= 0.05) {
+            change.NICKEL += 0.05;
+            change.centsTotal -= 0.05;
+        } else if (change.centsTotal >= 0.01) {
+            change.PENNY += 0.01;
+            change.centsTotal -= 0.01;
+        }
+    } while (change.centsTotal >= 0.01)
+
+    // tally dollar denominations until there is less than a dollar left
+    do {
+        if (change.dollarsTotal >= 100) {
+            change.ONEHUNDRED += 100;
+            change.dollarsTotal -= 100;
+        } else if (change.dollarsTotal >= 20) {
+            change.TWENTY += 20;
+            change.dollarsTotal -= 20;
+        } else if (change.dollarsTotal >= 10) {
+            change.TEN += 10;
+            change.dollarsTotal -= 10;
+        } else if (change.dollarsTotal >= 5) {
+            change.FIVE += 5;
+            change.dollarsTotal -= 5;
+        } else if (change.dollarsTotal >= 1) {
+            change.ONE += 1;
+            change.dollarsTotal -= 1;
+        }
+    } while ( change.dollarsTotal >= 1)
 
 }
 
